8da7d818f0cf8ad7a530f717e901cf4e3e238dd2
bayardozelaya <bayardozelaya@gmail.com>
2021-03-02 09:26:14 -0500

    solved twoSum Optimized

---
+++ Bayardo/twoSum.js
@@ -0,0 +1,37 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+const twoSum = (nums, target) => {
+  const hashNums = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (hashNums.has(complement)) {
+      return [i, hashNums.get(complement)];
+    }
+    hashNums.set(nums[i], i);
+  }
+};
+
+let nums = [2, 4, 11, 15],
+  target = 15;
+
+console.log(twoSum(nums, target));
